9d5a7b872d77cdf4af0d786e38badd3753946458
Zachry Weber <zachry.weber@gmail.com>
2023-10-04 00:27:00 -0600

    worky

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Mock-up Website</title>
-    <link rel="stylesheet" type="text/css" href="/css/index.css">
+    <title>stiggy</title>
 </head>
 <body>
+    <link rel="stylesheet" type="text/css" href="/css/index.css">
     <header>
         <h1>Welcome To Stiggy!</h1>
     </header>
