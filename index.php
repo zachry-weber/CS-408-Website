b42772955e5677a2a72b21f422319a2651348293
Zachry Weber <zachry.weber@gmail.com>
2023-10-03 22:34:01 -0600

    update trace

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Mock-up Website</title>
-    <link rel="stylesheet" type="text/css" href="/css/inedx.css">
+    <link rel="stylesheet" type="text/css" href="/css/index.css">
 </head>
 <body>
     <header>
