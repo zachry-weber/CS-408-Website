4fea6567f542b5d81431375932d8c639cb28fd9d
Zachry Weber <zachry.weber@gmail.com>
2023-10-04 00:14:46 -0600

    fix?

--- index.php
+++ index.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Mock-up Website</title>
-    <link rel="stylesheet" type="text/css" href="/css/index.css">
+    <style>
+        <link rel="stylesheet" type="text/css" href="/css/index.css">
+    </style>
 </head>
 <body>
     <header>
