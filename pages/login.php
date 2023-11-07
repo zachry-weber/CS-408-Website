3d19678deec6a053cfae9163bbb291fa0ef0632a
Zachry Weber <zachry.weber@gmail.com>
2023-11-06 21:31:44 -0700

    log sign

--- pages/login.php
+++ pages/login.php
@@ -1,1 +1,30 @@
-<?="Login"?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>stiggy</title>
+    <link rel="shortcut icon" href="../StiggyLogo.png">
+    <link rel="stylesheet" type="text/css" href="../css/index.css">
+</head>
+<body>
+    <header class="head">
+        <h1>Welcome To Stiggy!</h1>
+        <img src="../StiggyLogo.png" alt="logo" width="100" height="100" class="style-image">
+    </header>
+    <div class= "navb">
+        <a href="../index.php">Home</a>
+        <a href="/pages/design.php">Design</a>
+        <a href="/pages/gallery.php">Gallery</a>
+        <a href="/pages/about.php">About</a>
+        <a href="/pages/signup.php" class="split">Sign Up</a>
+        <a href="/pages/login.php" class="split">Login</a>
+    </div>
+    <div class="login">
+        <h2>Login</h2>
+    </div>
+    <footer>
+        &copy; 2023 Stiggy
+    </footer>
+</body>
+</html>
