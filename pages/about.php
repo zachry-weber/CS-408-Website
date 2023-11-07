3620cc190462e39f3f06bbaf57c184c35c4f6c65
Zachry Weber <zachry.weber@gmail.com>
2023-11-07 04:51:46 -0700

    update

--- pages/about.php
+++ pages/about.php
@@ -26,13 +26,13 @@ print_r($_SESSION);
         <a href="/pages/login.php" class="split">Login</a>
     </div>
     <div class="aboutMe">
-        <img src="../picofme.JPG" alt="me">
         <p>Hi my name is Zachry Weber, I am 23 years old and currently attend
             Boise State University. My hobbies include playing disc golf, ultimate
             frisbee, fishing, golfing, and hanging out with friends.
         </p>
     </div>
     <div class="meVideo">
+      <img src="../picofme.JPG" alt="me">
       <video controls loop>
       <source src="../discgolf.MP4" type="video/mp4">
       </video>  
