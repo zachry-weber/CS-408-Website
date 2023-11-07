bb4ede8e66678437079add553f0d659af725c9a7
Zachry Weber <zachry.weber@gmail.com>
2023-11-06 23:21:20 -0700

    yes

--- Dao.php
+++ Dao.php
@@ -6,9 +6,8 @@ class Dao {
   private $pass = "jr3iy2vu473w7pzb";
   public function getConnection () {
     return
-      new PDO("mysql:host={$this->host};dbname={$this->db}", $this->user,
-        $this->pass);
-      
+     new PDO("mysql:host={$this->host};dbname={$this->db}", $this->user,
+          $this->pass); 
   }
   
   public function newUser($name, $email, $password) {
