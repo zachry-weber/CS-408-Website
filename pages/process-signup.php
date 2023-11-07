d9bbab9927510c015a619cfeb96b62591b485280
Zachry Weber <zachry.weber@gmail.com>
2023-11-06 23:05:20 -0700

    fuck

--- pages/process-signup.php
+++ pages/process-signup.php
@@ -28,7 +28,7 @@ if ($_POST["password"] !== $_POST["password_confirmation"]) {
 
 $password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
 
-$dao = new Dao();
+//$dao = new Dao();
 
 echo ("fuck");
 
