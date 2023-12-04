002c72cb47dcd57b83bfb9abe0d6cff8aa908eaf
Zachry Weber <zachry.weber@gmail.com>
2023-12-03 19:02:13 -0700

    upload

--- pages/upload.php
+++ pages/upload.php
@@ -4,4 +4,33 @@ if (isset($_POST['submit'])) {
     $file = $_FILES['file'];
     print_r($file);
     $fileName = $_FILES['file']['name'];
+    $fileTmpName = $_FILES['file']['tmp_name'];
+    $fileSize = $_FILES['file']['size'];
+    $fileError = $_FILES['file']['error'];
+    $fileType = $_FILES['file']['type'];
+    
+    $fileExt = explode('.', $fileName);
+    $fileActualExt = strtolower(end($fileExt));
+
+    $allowed = array('jpg', 'jpeg', 'png');
+    if (in_array($fileActualExt, $allowed)) {
+        if ($fileError === 0) {
+            if ($fileSize < 320000) {
+                $fileNameNew = uniqid('', true).".".$fileActualExt;
+                $fileDestination = '../uploads/'.$fileNameNew;
+		move_uploaded_file($fileTmpName, $fileDestination);
+                header("Location: ../pages/design.php?uploadsuccess");
+            }
+            else {
+                echo "Your file is too big!";
+            }
+ 	}
+        else {
+             echo "There was an error uploading your file!";
+        }
+    }
+    else {
+       echo "You can't upload files of this type!";
+    }
+
 }
