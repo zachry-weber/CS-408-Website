3011de4c0cd21ffc20e6f7ec650b609a14ca05aa
Zachry Weber <zachry.weber@gmail.com>
2023-12-03 18:30:18 -0700

    upload

--- pages/design.php
+++ pages/design.php
@@ -30,7 +30,7 @@ print_r($_SESSION);
         /* <h2>Upload your photos here</h2> */
     </div>
     <div class="uploads">
-        <form action="upload.php" method "POST" entype="multipart/form-data">
+        <form action="upload.php" method "POST" enctype="multipart/form-data">
 	    <input type="file" name="file">
 	    <button type="submit" name="submit">Upload Image</button>
     	</form>
