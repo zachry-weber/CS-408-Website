bbd110ccfd7f3115c686234b42677e053340ee66
Zachry Weber <zachry.weber@gmail.com>
2023-12-06 05:06:34 -0700

    style

--- pages/login.php
+++ pages/login.php
@@ -13,15 +13,6 @@ session_start();
     <link rel="shortcut icon" href="../favicon.ico" type="image/x-icon">
     <link rel="stylesheet" type="text/css" href="../css/index.css">
     <style>
-        body {
-            font-family: 'Arial', sans-serif;
-            background-color: #f2f2f2;
-            margin: 0;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-        }
         .login {
             background-color: #ffffff;
             padding: 20px;
