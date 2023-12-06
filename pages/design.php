76fc5a5561f5a85d2b7e40720f2aa08b41d28ad6
Zachry Weber <zachry.weber@gmail.com>
2023-12-06 03:47:15 -0700

    please

--- pages/design.php
+++ pages/design.php
@@ -6,62 +6,36 @@ print_r($_SESSION);
 //require_once("/pages/upload.php");
 if (isset($_POST["submit"])) {
 
-    // Check image using getimagesize function and get size
-    // if a valid number is got then uploaded file is an image
-    if (isset($_FILES["image"])) {
-        // directory name to store the uploaded image files
-        // this should have sufficient read/write/execute permissions
-        // if not already exists, please create it in the root of the
-        // project folder
-        $targetDir = "uploads/";
-        $targetFile = $targetDir . basename($_FILES["image"]["name"]);
-        $uploadOk = 1;
-        $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+    if(isset($_FILES['image'])){
+      $errors= array();
 
-        $check = getimagesize($_FILES["image"]["tmp_name"]);
-        if ($check !== false) {
-            echo "File is an image - " . $check["mime"] . ".";
-            $uploadOk = 1;
-        } else {
-            echo "File is not an image.";
-            $uploadOk = 0;
-        }
-    }
+      $dir = "uploads/";
+      $file_name = $_FILES['image']['name'];
+      $file_name = $dir. $file_name;
+      $file_size = $_FILES['image']['size'];
+      $file_tmp = $_FILES['image']['tmp_name'];
+      $file_type = $_FILES['image']['type'];
+      $tmp = explode('.',$_FILES['image']['name']);
+      $file_ext=strtolower(end($tmp));
+      
+      $extensions= array("jpeg","jpg","png","gif");
+      
+      if(in_array($file_ext,$extensions)=== false){
+         $errors[]="extension not allowed, please choose a GIF, JPEG or PNG file.";
+      }
+      
+      if($file_size > 2097152) {
+         $errors[]='File size must be excately 2 MB';
+      }
+      
+      if(empty($errors)==true) {
+         move_uploaded_file($file_tmp, $file_name);
+         echo "Success";
+      }else{
+         print_r($errors);
+      }
+   }
 
-    // Check if the file already exists in the same path
-    if (file_exists($targetFile)) {
-        echo "Sorry, file already exists.";
-        $uploadOk = 0;
-    }
-
-    // Check file size and throw error if it is greater than
-    // the predefined value, here it is 500000
-    if ($_FILES["image"]["size"] > 500000) {
-        echo "Sorry, your file is too large.";
-        $uploadOk = 0;
-    }
-
-    // Check for uploaded file formats and allow only 
-    // jpg, png, jpeg and gif
-    // If you want to allow more formats, declare it here
-    if (
-        $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-        && $imageFileType != "gif"
-    ) {
-        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-        $uploadOk = 0;
-    }
-
-    // Check if $uploadOk is set to 0 by an error
-    if ($uploadOk == 0) {
-        echo "Sorry, your file was not uploaded.";
-    } else {
-        if (move_uploaded_file($_FILES["image"]["tmp_name"], $targetFile)) {
-            echo "The file " . htmlspecialchars(basename($_FILES["image"]["name"])) . " has been uploaded.";
-        } else {
-            echo "Sorry, there was an error uploading your file.";
-        }
-    }
 }
 
 ?>
@@ -102,8 +76,8 @@ if (isset($_POST["submit"])) {
 	    <button type="submit" name="submit">Upload Image</button>
     	</form>
         <h1>Display uploaded Image:</h1>
-        <?php if (isset($_FILES["image"]) && $uploadOk == 1) : ?>
-            <img src="<?php echo $targetFile; ?>" alt="Uploaded Image">
+        <?php if (isset($_FILES["image"])) : ?>
+            <img src="<?php echo $file_name; ?>" alt="Uploaded Image">
         <?php endif; ?>
     </div>
     <footer>
