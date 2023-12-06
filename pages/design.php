f1054ad0cd4f36701fa8118d315a5ac1518feab2
Zachry Weber <zachry.weber@gmail.com>
2023-12-06 12:53:20 -0700

    test

--- pages/design.php
+++ pages/design.php
@@ -3,6 +3,67 @@ session_start();
 print_r($_SESSION);
 
 //require_once("/pages/upload.php");
+if (isset($_POST["submit"])) {
+
+    // Check image using getimagesize function and get size
+    // if a valid number is got then uploaded file is an image
+    if (isset($_FILES["image"])) {
+        // directory name to store the uploaded image files
+        // this should have sufficient read/write/execute permissions
+        // if not already exists, please create it in the root of the
+        // project folder
+        $targetDir = "../uploads/";
+        $targetFile = $targetDir . basename($_FILES["image"]["name"]);
+        $uploadOk = 1;
+        $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+
+        $check = getimagesize($_FILES["image"]["tmp_name"]);
+        if ($check !== false) {
+            echo "File is an image - " . $check["mime"] . ".";
+            $uploadOk = 1;
+        } else {
+            echo "File is not an image.";
+            $uploadOk = 0;
+        }
+    }
+
+    // Check if the file already exists in the same path
+    if (file_exists($targetFile)) {
+        echo "Sorry, file already exists.";
+        $uploadOk = 0;
+    }
+
+    // Check file size and throw error if it is greater than
+    // the predefined value, here it is 500000
+    if ($_FILES["image"]["size"] > 500000) {
+        echo "Sorry, your file is too large.";
+        $uploadOk = 0;
+    }
+
+    // Check for uploaded file formats and allow only 
+    // jpg, png, jpeg and gif
+    // If you want to allow more formats, declare it here
+    if (
+        $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+        && $imageFileType != "gif"
+    ) {
+        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        $uploadOk = 0;
+    }
+
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+        echo "Sorry, your file was not uploaded.";
+    } else {
+        if (move_uploaded_file($_FILES["image"]["tmp_name"], $targetFile)) {
+            echo "The file " . htmlspecialchars(basename($_FILES["image"]["name"])) . " has been uploaded.";
+        } else {
+            echo "Sorry, there was an error uploading your file.";
+        }
+    }
+  //header("Location: /pages/gallery.php");
+  //exit();
+}
 
 ?>
 
@@ -89,7 +150,7 @@ print_r($_SESSION);
         <h2>Select an image you would like to display</h2>
     </div>
     <div class="uploads">
-        <form action="/pages/upload.php" method="POST" enctype="multipart/form-data">
+        <form action="" method="POST" enctype="multipart/form-data">
 	    <input type="file" name="image" id="file">
             <button type="submit" name="submit">Upload Image</button>
         </form>
