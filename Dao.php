4138c8e0d5634382f0e83814f764d2db4597dedc
Zachry Weber <zachry.weber@gmail.com>
2023-12-06 01:32:00 -0700

    test

--- Dao.php
+++ Dao.php
@@ -5,6 +5,8 @@ class Dao {
   private $user = "njmze3cticven8hp";
   private $pass = "jr3iy2vu473w7pzb";
   
+  $conn = mysqli_connect($host, $user, $pass, $db);
+
   public function getConnection () {
     return
      new PDO("mysql:host={$this->host};dbname={$this->db}", $this->user,
--- pages/upload.php
+++ pages/upload.php
@@ -24,7 +24,7 @@ if (isset($_POST['submit']) && isset($_FILES['my_image'])) {
 
 			if (in_array($img_ex_lc, $allowed_exs)) {
 				$new_img_name = uniqid("IMG-", true).'.'.$img_ex_lc;
-				$img_upload_path = 'uploads/'.$new_img_name;
+				$img_upload_path = '/uploads/'.$new_img_name;
 				move_uploaded_file($tmp_name, $img_upload_path);
 
 				// Insert into Database
